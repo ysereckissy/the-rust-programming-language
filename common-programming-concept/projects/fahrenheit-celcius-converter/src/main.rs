cf50d705247150777a84281e7b65ab61b70c1f62
Yannick Avelin Sereckissy-Namboy <yannick.sereckissy@gmail.com>
2025-01-23 11:34:10 +0100

    Refactored the main function
    Split logic in separate functions to ease code reading

--- common-programming-concept/projects/fahrenheit-celcius-converter/src/main.rs
+++ common-programming-concept/projects/fahrenheit-celcius-converter/src/main.rs
@@ -4,40 +4,27 @@ fn main() {
     loop {
         print_menu();
         let mut conversion_direction = String::new();
-        io::stdin()
-            .read_line(&mut conversion_direction)
-            .expect("Reading the conversion direction failed!");
+        console_read(&mut conversion_direction);
 
-        let conversion_direction: u32 = conversion_direction
-            .trim()
-            .parse()
-            .expect("Unable to parse the given conversion direction");
-
+        let conversion_direction = string_to_unsigned_integer(&conversion_direction);
         if conversion_direction == 0 {
             println!("--> You don't need anything more? Bye for now!");
             break;
         }
 
-        let direction_description = if conversion_direction == 1 {
-            "celcius --> fahrenheit"
-        } else if conversion_direction == 2 {
-            "fahrenheit --> celcius"
-        } else {
-            println!("unknown conversion direction");
-            continue;
+        let direction_description = match conversion_direction {
+            1 => "celcius --> fahrenheit",
+            2 => "fahrenheit --> celcius",
+            _ => {
+                println!("unknown conversion direction");
+                continue;
+            }
         };
 
         println!("Enter the value to convert: {direction_description}");
         let mut value_to_convert = String::new();
-
-        io::stdin()
-            .read_line(&mut value_to_convert)
-            .expect("Enable to read value to convert");
-        let value_to_convert: f32 = value_to_convert
-            .trim()
-            .parse()
-            .expect("Unable to deconde the value to convert");
-
+        console_read(&mut value_to_convert);
+        let value_to_convert = string_to_float(&value_to_convert);
         if conversion_direction == 1 {
             let converted_temperature = convert_from_celcius_to_fahrenheit(value_to_convert);
             println!("conversion of {value_to_convert} {direction_description} = {converted_temperature}");
@@ -61,4 +48,24 @@ fn convert_from_celcius_to_fahrenheit(temperature_celcius: f32) -> f32 {
 
 fn fahrenheit_to_celcius_converter(temperature_fahrenheit: f32) -> f32 {
     (temperature_fahrenheit - 32.0) / 1.8
+}
+
+fn console_read(console_read_buffer: &mut String) {
+    io::stdin()
+        .read_line(console_read_buffer)
+        .expect("Reading the conversion direction failed!");
+}
+
+fn string_to_unsigned_integer(string_value: &String) -> u32 {
+    string_value
+        .trim()
+        .parse()
+        .expect("Unable to parse the given conversion direction")
+}
+
+fn string_to_float(string_value: &String) -> f32 {
+    string_value
+        .trim()
+        .parse()
+        .expect("Unable to parse the given conversion direction")
 }
